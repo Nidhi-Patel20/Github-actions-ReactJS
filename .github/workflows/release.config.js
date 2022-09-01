a0ffafd632a38eb630040e246c3d540854a54e55
Nidhi Patel <NidPatel@eagle.org>
2022-09-01 16:31:27 +0530

    wf

--- .github/workflows/release.config.js
+++ .github/workflows/release.config.js
@@ -2,7 +2,9 @@ module.exports ={
     branches: "main",
     repositoryUrl: "https://github.com/Nidhi-Patel20/Github-actions-ReactJS",
     plugins: [
-        '@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', 
-        '@semantic-release/npm', '@semantic-release/github'
+        '@semantic-release/commit-analyzer', 
+        '@semantic-release/release-notes-generator', 
+        '@semantic-release/npm', 
+        '@semantic-release/github'
     ]
 }
