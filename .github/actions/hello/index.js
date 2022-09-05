b2cbf84c350cf9def97ffc01a56f6ee66402ca27
Nidhi Patel <NidPatel@eagle.org>
2022-09-05 18:53:54 +0530

    Own Actions:core functionalities

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,15 +2,25 @@ const core = require('@actions/core');
 const github =require('@actions/github');
 try{
 //throw new Error("some error message..");
+ 
+  core.debug('Debugging enabled-->Debug message');
+  core.warning('Warning message in orange');
+  core.error('Error message in red');
+
   const name =core.getInput('who-to-greet');
+  core.setSecret('name');  //will not appear in logs
+
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup('Logging github object');
   console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup();
 
-  core.setFailed('failed');
+  core.exportVariable("Hello","hello");
+
 } catch(error) {
   core.setFailed(error.message);
 }
