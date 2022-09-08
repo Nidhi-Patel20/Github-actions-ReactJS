ce52d1ef8b91dc61d7fb166a1bac27fa0eaee477
Nidhi Patel <NidPatel@eagle.org>
2022-09-08 11:30:52 +0530

    Update script.php

--- .github/actions/slack/script.php
+++ .github/actions/slack/script.php
@@ -3,48 +3,57 @@
 require_once 'vendor/autoload.php';
 Requests::register_autoloader();
 
-var_dump($argv);
-var_dump($_ENV);
+//var_dump($argv);
+//var_dump($_ENV);
 
+echo "::debug ::Sending a request to slack\n";
 
-// $response = Requests::post(
-//      "https://hooks.slack.com/services/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",
-//     array(
-//         'Content-type' => 'application/json'
-//     ),
-//     json_encode(array(
+echo 'Just a message';
+
+$response = Requests::post(
+     "https://hooks.slack.com/services/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",
+    array(
+        'Content-type' => 'application/json'
+    ),
+    json_encode(array(
         
-//         'blocks' => 
-//         array (
-//             array (
-//                 "type" => "section",
-//                 "text" => array (
-//                     "type" => "mrkdwn",
-//                     "text" => "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*",
-//                 ),
+        'blocks' => 
+        array (
+            array (
+                "type" => "section",
+                "text" => array (
+                    "type" => "mrkdwn",
+                    "text" => "*Event:*\n{$_ENV['GITHUB_EVENT_NAME']}"
+                    #"You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*",
+                ),
                 
-//                 ),
-//             ),
-//             array (
-//                 "type" => "section",
-//                 "fields" => array (
-//                     array (
-//                         "type" => "mrkdwn",
-//                         "text" => "*Type:*\nComputer (laptop)",
-//                     ),
-//                     array (
-//                         "type" => "mrkdwn",
-//                         "text" => "*When:*\nSubmitted Aut 10",
-//                     ),
+                ),
+            ),
+            array (
+                "type" => "section",
+                "fields" => array (
+                    array (
+                        "type" => "mrkdwn",
+                        "text" => #"*Type:*\nComputer (laptop)",
+                        "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",
+                    ),
+                    array (
+                        "type" => "mrkdwn",
+                        "text" => "*SHA:*\n{$_ENV['GITHUB_SHA']}",
+                    ),
  
-//                 ),
-//             ),
-//          )
+                ),
+            ),
+         )
         
-//         )
-// );
-// var_dump($response);
+        )
+);
 
-// if(!$response->success) {
-//     echo $response->body;
-// }
+echo "::group::Slack Response\n";
+var_dump($response);
+echo '::endgroup::\n';
+
+if(!$response->success) {
+    echo $response->body;
+    exit(1);
+}
